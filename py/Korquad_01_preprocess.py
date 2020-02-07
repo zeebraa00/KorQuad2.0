e5aeef2a4baf6e7e4d03926eff90e79c04fd8a06
zeebraa000 <zeebraa00@gmail.com>
2020-02-07 11:58:01 +0900

    [add] first commit

---
+++ py/Korquad_01_preprocess.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import json
+import pandas as pd
+
+
+# ## KorQuad 1.0
+
+# In[4]:
+
+
+corpus_fname_01 = "./data/input/KorQuAD_v1.0_train.json"
+output_fname_01 = "./data/output/korquad_v01.txt"
+
+
+# In[5]:
+
+
+with open(corpus_fname_01) as f1, open(output_fname_01, 'w', encoding ='utf-8') as f2:
+    dataset_json = json.load(f1)
+    dataset = dataset_json['data']
+    for article in dataset:
+        w_lines = []
+        for paragraph in article['paragraphs']:
+            w_lines.append(paragraph['context'])
+            for qa in paragraph['qas']:
+                q_text = qa['question']
+                for a in qa['answers']:
+                    a_text = a['text']
+                    w_lines.append(q_text + " " + a_text)
+        for line in w_lines:
+            f2.writelines(line + '\n')
+
+
+# In[6]:
+
+
+dataset[0]
+
+
+# In[54]:
+
+
+w_lines
+
+
+# ## KorQuad 2.0
+
+# In[46]:
+
+
+corpus_fname_02 = "./data/input/korquad2.1_train_00.json"
+output_fname_02 = "./data/output/processed_korquad_train_00.txt"
+
+
+# In[47]:
+
+
+with open(corpus_fname_02) as f1, open(output_fname_02, 'w', encoding ='utf-8') as f2:
+    dataset_json_02 = json.load(f1)
+    dataset_02 = dataset_json_02['data']
+    for article in dataset_02:
+        w_lines = []
+        '''for paragraph in article['paragraphs']:
+            w_lines.append(paragraph['context'])
+            for qa in paragraph['qas']:
+                q_text = qa['question']
+                for a in qa['answers']:
+                    a_text = a['text']
+                    w_lines.append(q_text + " " + a_text)'''
+        for line in w_lines:
+            f2.writelines(line + '\n')
+                    
+
+
+# In[48]:
+
+
+dataset_02 = dataset_json_02['data']
+
+
+# In[49]:
+
+
+dataset_json_02.keys()
+
+
+# In[50]:
+
+
+dataset_json_02['data'][0]
+
+
+# In[51]:
+
+
+dataset_json_02['data'][1]
+
+
+# In[42]:
+
+
+len(dataset_json['data'])
+
